78b823db37b77b340e63189a5595829065b2d7d0
Betty Mutai <bettymutai00@gmail.com>
2017-07-12 16:51:33 +0300

    Added specs helper to avoid repetion on code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,11 @@ require('rspec')
   require('patron')
   require('book')
 
-  
+  DB = PG.connect({:dbname => 'library_system_test'})
+
+  RSpec.configure do |config|
+    config.after(:each) do
+      DB.exec("DELETE FROM patrons *;")
+      DB.exec("DELETE FROM books *;")
+    end
+  end
