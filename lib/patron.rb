5ae96dee318439f934df732dfff1411eeb301791
Betty Mutai <bettymutai00@gmail.com>
2017-07-12 16:46:34 +0300

    Added a comparison method to find similar patrons and a find method to find a specific patron using their id and also added a books method to link patron and book class

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,19 +1,51 @@
 class Patron
   attr_reader(:name, :id)
 
-  @@all_patrons = []
-
-  define_method(:initialize) do |attributtes|
-    @name = attributtes.fetch(:name)
-    @id = attributtes.fetch(:id)
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
   end
 
   define_singleton_method(:all) do
-    @@all_patrons
-  end
+    returned_patrons = DB.exec("SELECT * FROM patrons;")
+      patrons = []
+      returned_patrons.each() do |patron|
+        name = patron.fetch("name")
+        id = patron.fetch("id").to_i()
+        patrons.push(Patron.new({:name => name, :id => id}))
+      end
+      patrons
+    end
 
   define_method(:save) do
-    @@all_patrons.push(self)
+    result = DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_patron|
+    self.name().==(another_patron.name()).&(self.id().==(another_patron.id()))
+  end
+
+  define_singleton_method(:find) do |id|
+    found_patron = nil
+    Patron.all().each() do |patron|
+      if patron.id().==(id)
+        found_patron = patron
+      end
+    end
+    found_patron
+  end
+
+  define_method(:books) do
+    patron_books = []
+    books = DB.exec("SELECT * FROM books WHERE patron_id = #{self.id()};")
+    books.each() do |book|
+      title = book.fetch("title")
+      author = book.fetch("author")
+      patron_id = book.fetch("patron_id").to_i()
+      patron_books.push(Book.new({:title => title, :author => author, :patron_id => patron_id}))
+    end
+    patron_books
   end
 
 
