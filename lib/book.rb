95f28e374c51d94e9d458cd18fe91c6041c3b832
Betty Mutai <bettymutai00@gmail.com>
2017-07-12 16:38:52 +0300

    Added a method to save new entries in my database and also compare same books

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,31 @@
 class Book
-  @@all_books = []
 
   attr_reader(:title, :author, :patron_id)
 
-  define_method(:initialize) do |attributtes|
-    @title = attributtes.fetch(:title)
-    @author = attributtes.fetch(:author)
-    @patron_id = attributtes.fetch(:patron_id)
+  define_method(:initialize) do |attributes|
+    @title = attributes.fetch(:title)
+    @author = attributes.fetch(:author)
+    @patron_id = attributes.fetch(:patron_id)
   end
 
   define_singleton_method(:all) do
-    @@all_books
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |book|
+      title = book.fetch("title")
+      author = book.fetch("author")
+      patron_id = book.fetch("patron_id").to_i()
+      books.push(Book.new({:title => title, :author => author, :patron_id => patron_id}))
+    end
+    books
   end
 
   define_method(:save) do
-    @@all_books.push(self)
+    DB.exec("INSERT INTO books (title, author, patron_id) VALUES ('#{@title}', '#{@author}', #{@patron_id});")
+  end
+
+  define_method(:==) do |another_book|
+    self.title().==(another_book.title())&(self.author().==(another_book.author()))&(self.patron_id().==(another_book.patron_id()))
   end
 
 end
